077b285770c87529ba1cb50a4a336c9841bc7249
ryu sugiyama <2005ryu427@gmail.com>
2025-06-16 17:04:51 +0900

    class 実装

--- main.py
+++ main.py
@@ -15,3 +15,34 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain.schema import Document
 
+class RouteQuery(BaseModel):
+    """ユーザーのクエリを最も関連性の高いデータソースにルーティングします。"""
+
+    datasource: Literal["vectorstore", "web_search"] = Field(
+        ...,
+        description="ユーザーの質問に応じて、ウェブ検索またはベクターストアにルーティングします。",
+    )
+
+
+class GradeDocuments(BaseModel):
+    """取得された文書の関連性チェックのためのバイナリスコア。"""
+
+    binary_score: str = Field(
+        description="文書が質問に関連しているかどうか、「yes」または「no」"
+    )
+
+
+class GradeHallucinations(BaseModel):
+    """生成された回答における幻覚の有無を示すバイナリスコア。"""
+
+    binary_score: str = Field(
+        description="回答が事実に基づいているかどうか、「yes」または「no」"
+    )
+
+class GradeAnswer(BaseModel):
+    """回答が質問に対処しているかどうかを評価するバイナリスコア。"""
+
+    binary_score: str = Field(
+        description="回答が質問に対処しているかどうか、「yes」または「no」"
+    )
+
